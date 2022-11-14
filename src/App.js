9159d66e702999861a72d695d5d1758e1ac4a6d6
aradhya8 <113983446+aradhya8@users.noreply.github.com>
2022-11-14 09:32:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './App.css';
+import Formcomponent from './Formcomponent'
+function App() {
+return(
+<Formcomponent>
+</Formcomponent>
+  )
+  }
+export default App;
